3135bcd32234ef9d714c3bcd4c7b0d6302903789
Ivan Marosan <37896581+IvanMarosan@users.noreply.github.com>
2019-11-11 13:26:52 +0100

    Hotfix - Presets
    Made sure so one couldn't press the preset buttons while the microwave is running

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -99,37 +99,42 @@ public class Controller {
 
     @FXML
     void defrostButtonPressed() {
-        SettingsSingleton.getInstance().setWattage(500);
-        SettingsSingleton.getInstance().setTime(120);
-        currentWattageLabel.setText(Integer.toString(SettingsSingleton.getInstance().getWattage()));
-        remainingTimeLabel.setText(Integer.toString(SettingsSingleton.getInstance().getTime()));
-
+        if (!th.isAlive()) {
+            SettingsSingleton.getInstance().setWattage(500);
+            SettingsSingleton.getInstance().setTime(120);
+            currentWattageLabel.setText(Integer.toString(SettingsSingleton.getInstance().getWattage()));
+            remainingTimeLabel.setText(Integer.toString(SettingsSingleton.getInstance().getTime()));
+        }
     }
     @FXML
     void fishButtonPressed() {
-        SettingsSingleton.getInstance().setWattage(800);
-        SettingsSingleton.getInstance().setTime(180);
-        currentWattageLabel.setText(Integer.toString(SettingsSingleton.getInstance().getWattage()));
-        remainingTimeLabel.setText(Integer.toString(SettingsSingleton.getInstance().getTime()));
+        if (!th.isAlive()) {
+            SettingsSingleton.getInstance().setWattage(800);
+            SettingsSingleton.getInstance().setTime(180);
+            currentWattageLabel.setText(Integer.toString(SettingsSingleton.getInstance().getWattage()));
+            remainingTimeLabel.setText(Integer.toString(SettingsSingleton.getInstance().getTime()));
 
-
+        }
     }
     @FXML
     void beefButtonPressed() {
-        SettingsSingleton.getInstance().setWattage(600);
-        SettingsSingleton.getInstance().setTime(360);
-        currentWattageLabel.setText(Integer.toString(SettingsSingleton.getInstance().getWattage()));
-        remainingTimeLabel.setText(Integer.toString(SettingsSingleton.getInstance().getTime()));
-
+        if (th.isAlive()) {
+            SettingsSingleton.getInstance().setWattage(600);
+            SettingsSingleton.getInstance().setTime(360);
+            currentWattageLabel.setText(Integer.toString(SettingsSingleton.getInstance().getWattage()));
+            remainingTimeLabel.setText(Integer.toString(SettingsSingleton.getInstance().getTime()));
+        }
 
     }
     @FXML
     void poultryButtonPressed() {
-        SettingsSingleton.getInstance().setWattage(750);
-        SettingsSingleton.getInstance().setTime(240);
-        currentWattageLabel.setText(Integer.toString(SettingsSingleton.getInstance().getWattage()));
-        remainingTimeLabel.setText(Integer.toString(SettingsSingleton.getInstance().getTime()));
+        if (th.isAlive()) {
+            SettingsSingleton.getInstance().setWattage(750);
+            SettingsSingleton.getInstance().setTime(240);
+            currentWattageLabel.setText(Integer.toString(SettingsSingleton.getInstance().getWattage()));
+            remainingTimeLabel.setText(Integer.toString(SettingsSingleton.getInstance().getTime()));
 
+        }
 
     }
 
